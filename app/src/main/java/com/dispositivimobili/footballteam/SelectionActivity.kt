2b47697fd90255d5ae76bf1790f27b647dbaaad5
MattiaDema <mattiademaddalena@gmail.com>
2021-04-20 11:11:51 +0200

    Collegamento prime activity

--- app/src/main/java/com/dispositivimobili/footballteam/SelectionActivity.kt
+++ app/src/main/java/com/dispositivimobili/footballteam/SelectionActivity.kt
@@ -3,21 +3,25 @@ package com.dispositivimobili.footballteam
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_selection.*
 
 class SelectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selection)
-    }
 
-    fun onClickRegister(v: View){
-        val intentRegister = Intent(this, RegisterActivity::class.java)
-        startActivity(intentRegister)
-    }
+        buttonLogin.setOnClickListener{
+            val intent = Intent(this@SelectionActivity, LoginActivity::class.java)
+            intent.putExtra("selectionActivity", 0)
+            startActivityForResult(intent, 1)
+        }
 
-    fun onClickLogin(v: View){
-        val intentRegister = Intent(this, LoginActivity::class.java)
-        startActivity(intentRegister)
+        buttonRegister.setOnClickListener{
+            val intent = Intent(this@SelectionActivity, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
